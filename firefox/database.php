8ae90d9b5d032dc9dcd0812df3abbdb095ce2c5f
zhangbinic <308964132@qq.com>
2015-02-10 20:49:59 +0800

    表单创建数据表代码更新

--- firefox/database.php
+++ firefox/database.php
@@ -25,14 +25,30 @@ foreach($fieldarr as $v)
 }
 // echo $sql;
 $sql .= '`id` int(11) NOT NULL AUTO_INCREMENT,PRIMARY KEY (`id`)';
-$executesql = "DROP TABLE IF EXISTS `{$tablename}`;CREATE TABLE `{$tablename}` ({$sql}) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='{$tablename}';";
+$executesql = "DROP TABLE IF EXISTS `{$tablename}`;
+CREATE TABLE `{$tablename}` ({$sql}) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='{$tablename}';";
 // print_r($fieldarr);
 // echo $executesql;
 file_put_contents("E:/1.sql", $executesql);
-mysql_connect('localhost:3310','root','bhxz');
+// mysql_connect('localhost:3310','root','bhxz');
+mysql_connect('localhost','root','111111');
 mysql_select_db('test');
 mysql_query('set names utf8');
 $content = file_get_contents('E:/1.sql');
-// echo $content;die;
-$result = mysql_query($content);
-var_dump($result);
+//$content = explode('\r\n',$content);
+
+$content = str_replace("\r", "\n", $content);
+$content = explode(";\n", $content);
+
+//echo '<pre>';
+//print_r($content);die;
+foreach($content as $sql)
+{
+	$result = mysql_query($sql);
+	//var_dump($result);
+}
+if($result)
+{
+	$results['info'] = 'success';
+}
+echo json_encode($results);
